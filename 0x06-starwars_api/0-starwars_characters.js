c02b098e526b5fc12e09c5ad9a6d1a7a9a21e3c8
kafilat1210 <cafylhearty@gmail.com>
2024-11-17 16:34:10 +0000

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,3 +1,4 @@
+cat star.js                                                                                       
 #!/usr/bin/node
 // star wars api using nodejs
 
@@ -6,18 +7,31 @@ const request = require('request');
 const movieId = process.argv[2];
 const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-function sendReq (characterName, index) {
-  if (characterName.length === index) {
+function sendReq (characterNames, index) {
+  if (index >= characterNames.length) {  // Ensure we don't go out of bounds
     return;
   }
 
-  request(characterName[index], (error, response, body) => {
+  const characterUrl = characterNames[index];  // Get the character URL
+
+  if (!characterUrl) {
+    console.log(`No URL found for character at index ${index}`);
+    sendReq(characterNames, index + 1);  // Skip to the next character
+    return;
+  }
+
+  request(characterUrl, (error, response, body) => {
     if (error) {
       console.log(error);
     } else {
-      console.log(JSON.parse(body).name);
-      sendReq(characterName, index + 1);
+      try {
+        const character = JSON.parse(body);
+        console.log(character.name);  // Log the character's name
+      } catch (err) {
+        console.log(`Error parsing character data at index ${index}:`, err);
+      }
     }
+    sendReq(characterNames, index + 1);  // Proceed to the next character
   });
 }
 
@@ -25,8 +39,17 @@ request(movieEndpoint, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const characterName = JSON.parse(body).characters;
+    try {
+      const movieData = JSON.parse(body);
+      const characterNames = movieData.characters;  // Get the list of character URLs
 
-    sendReq(characterName, 0);
+      if (Array.isArray(characterNames) && characterNames.length > 0) {
+        sendReq(characterNames, 0);  // Start processing characters
+      } else {
+        console.log('No characters found for this movie.');
+      }
+    } catch (err) {
+      console.log('Error parsing movie data:', err);
+    }
   }
 });
