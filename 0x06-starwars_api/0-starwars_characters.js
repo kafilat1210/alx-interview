5cf58f5d21eebae60515178f6701849590273d08
kafilat1210 <cafylhearty@gmail.com>
2024-11-18 00:13:56 +0000

    update api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,31 +6,18 @@ const request = require('request');
 const movieId = process.argv[2];
 const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-function sendReq (characterNames, index) {
-  if (index >= characterNames.length) {  // Ensure we don't go out of bounds
+function sendReq (characterName, index) {
+  if (characterName.length === index) {
     return;
   }
 
-  const characterUrl = characterNames[index];  // Get the character URL
-
-  if (!characterUrl) {
-    console.log(`No URL found for character at index ${index}`);
-    sendReq(characterNames, index + 1);  // Skip to the next character
-    return;
-  }
-
-  request(characterUrl, (error, response, body) => {
+  request(characterName[index], (error, response, body) => {
     if (error) {
       console.log(error);
     } else {
-      try {
-        const character = JSON.parse(body);
-        console.log(character.name);  // Log the character's name
-      } catch (err) {
-        console.log(`Error parsing character data at index ${index}:`, err);
-      }
+      console.log(JSON.parse(body).name);
+      sendReq(characterName, index + 1);
     }
-    sendReq(characterNames, index + 1);  // Proceed to the next character
   });
 }
 
@@ -38,17 +25,8 @@ request(movieEndpoint, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    try {
-      const movieData = JSON.parse(body);
-      const characterNames = movieData.characters;  // Get the list of character URLs
+    const characterName = JSON.parse(body).characters;
 
-      if (Array.isArray(characterNames) && characterNames.length > 0) {
-        sendReq(characterNames, 0);  // Start processing characters
-      } else {
-        console.log('No characters found for this movie.');
-      }
-    } catch (err) {
-      console.log('Error parsing movie data:', err);
-    }
+    sendReq(characterName, 0);
   }
 });
